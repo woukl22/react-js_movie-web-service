b763df058e3113bc9e42d23f333213003308eaef
woukl22 <woukl22@gmail.com>
2021-10-02 13:58:17 +0000

    Ready to start making movie-web-service.

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -3,22 +3,18 @@ import PropTypes from "prop-types";
 
 class App extends React.Coponent{
 	state = {
-		count: 0
+		isLoading: true,
+		movies: []
 	};
-	add = () => {
-		this.setState(current => ({ count: current.count + 1 }));
-	};
-	minus = () => {
-		this.setState(current => ({ count: current.count - 1 }));
-	};
+	componentDidMount(){
+		setTimeout(() => {
+			this.setState({ isLoading: false });
+		}, 6000);
+	}
 	render() {
-		return 
-		<div> 
-			<h1>Im a class {this.state.count}</h1>
-			<button onClick={this.add}>Add</button>
-			<button onClick={this.minus}>Minus</button>
-		</div>
-	};
+		const { isLoading } = this.state;
+		return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+	}
 }
 
 export default App;
