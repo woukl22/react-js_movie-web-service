efb5a85a5bf4704d35adba2b126e24d37794516c
woukl22 <woukl22@gmail.com>
2021-10-03 00:00:11 +0000

    Styling 'App.css' and 'Movie.css'

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -18,13 +18,13 @@ class App extends React.Coponent{
 	render() {
 		const { isLoading, movies } = this.state;
 		return (
-			<section class="container">
+			<section className="container">
 				{isLoading ? (
-					<div class="loader">
-						<span class="loader__text">Loading...</span>
+					<div className="loader">
+						<span className="loader__text">Loading...</span>
 					</div>
 				) : (
-					<div class="movies">
+					<div className="movies">
 						{movies.map(movie => (
 							<Movie 
 								key={movie.id}
@@ -32,7 +32,8 @@ class App extends React.Coponent{
 								year={movie.year} 
 								title={movie.title} 
 								summary={movie.summary} 
-								poster={movie.medium_cover_image} 
+								poster={movie.medium_cover_image}
+								genres={movie.genres}
 							/>
 						))}
 					</div>
